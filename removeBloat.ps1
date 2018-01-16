471cda7552811c8cf40c7167feb17975dd313362
dratreset <re.thompson3@gmail.com>
2018-01-16 15:36:14 -0700

    Add files via upload

---
+++ removeBloat.ps1
@@ -0,0 +1,65 @@
+#############
+#
+#	removeBloat_v1.2.ps1
+#	Author: Rob Thompson
+#
+#	Description:	Removes unnecessary bloatware from a base Win10 install; includes normal and provisioned apps.
+#
+#	Notes:		-"XBoxCallableUI" may error and not be removed. I have not found a solution to this specific app
+#			
+#			-If you want to keep a specific app, simply comment out the line contianing the app in the array below. Doing this will not affect the functionality.
+#				--This script by default has kept Alarms, Calculator, MSPaint, Photos, and Sticky notes.
+#				--Most of the apps names are pretty self explanatory. If you aren't sure, check online.
+#			
+#			-The second loop in this script removes PROVISIONED apps as well; to use, uncomment the section by removing the <# in the front, and #> at the end.
+#
+#############
+
+
+$bloatWare = @(`
+"*3dbuilder*", ` 
+#"*alarms*", `
+"*appconnector*", `
+"*appinstaller*", `
+"*bing*", `
+"*bingfinance*", `
+"*bingnews*", `
+"*bingsports*", `
+"*bingweather*", `
+#"*calculator*", `
+"*camera*", `
+"*commsphone*", `
+"*communicationsapps*", `
+"*connectivitystore*", `
+"*feedback*", `
+"*getstarted*", `
+"*holographic*", `
+"*maps*", `
+"*messaging*", `
+#"*mspaint*", `
+"*officehub*", `
+"*oneconnect*", `
+"*onenote*", `
+"*people*", `
+"*phone*", `
+#"*photos*", `
+"*skypeapp*", `
+"*solitaire*", `
+"*soundrecorder*", `
+#"*sticky*", `
+"*sway*", `
+"*wallet*", `
+"*windowsphone*", `
+"*windowsstore*", `
+"*xbox*", `
+"*zune*", `
+"*zunemusic*", `
+"*zunevideo*")
+
+for ($i=0; $i -lt $bloatware.length; $i++) {
+	Get-AppxPackage -name $bloatware[$i] | Remove-AppxPackage
+}
+
+<#for ($j=0; $j -lt $bloatWare.length; $j++) {
+    Get-AppxProvisionedPackage -Online | Where-Object PackageName -like $bloatWare[$j] | Remove-AppxProvisionedPackage -Online
+}#>
